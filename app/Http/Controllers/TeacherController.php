785c539860d6e07e1916df61e250182cb109e8df
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2022-07-03 00:19:05 +0200

    make routes

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    public function index()
+    {
+        dd("this is index");
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function store()
+    {
+        
+    }
+
+    public function edit($id)
+    {
+        
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function destroy()
+    {
+        
+    }
+}
